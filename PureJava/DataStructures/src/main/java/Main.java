dd77bdef61eb1b0a4463b42c861c6ea1c79dbe33
AndresNamm <namm@connect.hku.hk>
2017-08-05 18:45:59 +0300

    Added comparator to DS

--- PureJava/DataStructures/src/main/java/Main.java
+++ PureJava/DataStructures/src/main/java/Main.java
@@ -15,6 +15,19 @@ public class Main {
         }
     }
 
+    public class ComparePoint implements Comparator<Integer[]>{
+        @Override
+        public int compare(Integer[] o1, Integer[] o2) {
+            if(o1[0]>o2[0]){
+                return 1;
+
+            } else if(o1[0]<o2[0]) {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
     public void arrayListExample() {
 
         //Regular Integer
